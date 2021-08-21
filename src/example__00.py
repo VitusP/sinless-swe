31e09817ddde7b835ed85a32bfee844b9f6d47f7
vaputra <vaputra@ncsu.edu>
2021-08-21 16:10:39 -0400

    Update commenting style

--- src/example__00.py
+++ src/example__00.py
@@ -1,20 +1,26 @@
-'''
+"""
 This is a sample python script used to test the Github Workflow
 All of the requirements are listed in requirements.txt
-'''
+"""
 
 import math
 
-'''
+"""
 Function to get square root
-'''
+
+:param: none
+:return: sqrt of a number
+"""
 def my_sqrt():
     num = 25
     return sqrt(num)
 
-'''
+"""
 Function to get a square
-'''
+
+:param: none
+:return: sqrt of a number
+"""
 def my_square():
     num = 10
     return num*num
