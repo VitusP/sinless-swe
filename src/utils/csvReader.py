6685b816f5a7e6549d2d79969678adcafaf25fc9
vaputra <vaputra@ncsu.edu>
2021-08-29 15:36:35 -0400

    hw2-Update the csv file reader

--- src/utils/csvReader.py
+++ src/utils/csvReader.py
@@ -1,7 +1,5 @@
 import sys
 import re
-import csv
-
 
 # def csv_reader(fileName):    
 #     try:
@@ -23,6 +21,7 @@ import csv
 #     else:
 #         print("Column Length: ", columnsLength)
 
+## Function to read CSV data and output the list of list
 def csv_reader(fileName):    
     try:
         with open(fileName, newline='') as f:
@@ -48,22 +47,22 @@ def csv_reader(fileName):
                     currentColumn = 0
                     newRow = []
                     for column in row:
-                        print("Before conversion: ", column)
+                        # print("Before conversion: ", column)
                         isNumber = dataTypeMap[currentColumn]
-                        print("Is number?: ", isNumber)
+                        # print("Is number?: ", isNumber)
                         if isNumber == True:
                             try:
                                 newRow.append(float(column))
                             except Exception as e:
                                 # Failed to convert to float
-                                print(e)
+                                # print(e)
                                 rowIsValid = False
                                 break
                         else:
                             newRow.append(column)
-                        print(column)
+                        # print(column)
                         currentColumn += 1
-                    print("New Row: ", newRow)
+                    # print("New Row: ", newRow)
                     row = newRow
                 # Check if each row has correct column length
                 if len(row) == columnLength and rowIsValid:
@@ -71,10 +70,10 @@ def csv_reader(fileName):
     except Exception:
         print("Failed to read csv")
     else:
-        print("")
         return listofRows
 
 fileName = sys.argv[1]
 dataList = csv_reader(fileName)
 for row in dataList:
     print(row)
+print("Length of data", len(dataList))
