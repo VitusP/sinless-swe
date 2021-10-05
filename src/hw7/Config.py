90e079b25df73ac7072ccb8f64c99662f3c718dd
Hatsukikeika <xuegao60189@gmail.com>
2021-10-05 17:53:53 -0400

    rename variable, add init value

--- src/hw7/Config.py
+++ src/hw7/Config.py
@@ -3,11 +3,11 @@ import random
 
 class Config:
 
-    p, enough, samples, far, cohen, biins, support = None, None, None, None, None, None, None
+    p, enough, samples, far, cohen, biins, support = 2, 0.4, 128, 0.9, 0.35, 0.4, 2
 
     def __init__(self):
 
-        self.dict = {
+        self.bond = {
         'p': (2, 3),
         'enough': (0.4, 0.6),
         'samples': (6, 8),
@@ -18,18 +18,18 @@ class Config:
         }
 
     def set_bond(self, key, low, hi):
-        if key not in self.dict.keys():
+        if key not in self.bond.keys():
             Exception
         if low > hi:
             Exception
-        self.dict[key] = (low, hi)
+        self.bond[key] = (low, hi)
 
     def build(self):
 
-        self.__class__.p = random.randrange(self.dict['p'][0], self.dict['p'][1], 1)
-        self.__class__.enough = random.uniform(self.dict['enough'][0], self.dict['enough'][1])
-        self.__class__.samples = 2**random.randrange(self.dict['samples'][0], self.dict['samples'][1], 1)
-        self.__class__.far = random.uniform(self.dict['far'][0], self.dict['far'][1])
-        self.__class__.cohen = random.uniform(self.dict['cohen'][0], self.dict['cohen'][1])
-        self.__class__.bins = random.uniform(self.dict['bins'][0], self.dict['bins'][1])
-        self.__class__.support = random.randrange(self.dict['support'][0], self.dict['support'][1], 1)
+        self.__class__.p = random.randrange(self.bond['p'][0], self.bond['p'][1], 1)
+        self.__class__.enough = random.uniform(self.bond['enough'][0], self.bond['enough'][1])
+        self.__class__.samples = 2**random.randrange(self.bond['samples'][0], self.bond['samples'][1], 1)
+        self.__class__.far = random.uniform(self.bond['far'][0], self.bond['far'][1])
+        self.__class__.cohen = random.uniform(self.bond['cohen'][0], self.bond['cohen'][1])
+        self.__class__.bins = random.uniform(self.bond['bins'][0], self.bond['bins'][1])
+        self.__class__.support = random.randrange(self.bond['support'][0], self.bond['support'][1], 1)
